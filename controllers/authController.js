b6c6f7961ee0a5d4cabb8c30deb185cf42bb2e9d
mohmousad <moh.moussad@gmail.com>
2024-05-27 22:46:38 +0300

    Add: Register and Register admin endpoints

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,62 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { CustomError } = require("../middlewares/errorHandler");
 
+exports.register = async (req, res) => {
+  const { name, phone, password, role, status } = req.body;
+
+  if (role.toLowerCase() == "admin") {
+    throw new CustomError({ type: "AuthorizationError" });
+  }
+
+  const existingUser = await User.findOne({ phone });
+  if (existingUser) {
+    throw new CustomError({ type: "ConflictError" });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = new User({
+    name,
+    phone,
+    password: hashedPassword,
+    role: role.toUpperCase(),
+    status,
+  });
+
+  await user.save();
+
+  const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET);
+
+  res.status(201).send({ token });
+};
+
+exports.registerAdmin = async (req, res) => {
+  const { name, phone, password, status } = req.body;
+
+  const existingUser = await User.findOne({ phone });
+  if (existingUser) {
+    throw new CustomError({ type: "ConflictError" });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = new User({
+    name,
+    phone,
+    password: hashedPassword,
+    role: "ADMIN",
+    status,
+  });
+
+  await user.save();
+
+  const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET);
+
+  res.status(201).send({ token });
+};
+
 exports.login = async (req, res) => {
   const { phone, password } = req.body;
   const user = await User.findOne({ phone });
--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 const controller = require("../utils/controller");
+const authorize = require("../middlewares/authorize");
 const { validate, loginValidationRules, registerValidationRules, registerAdminValidationRules } = require("../middlewares/validate");
 
+router.post("/register", validate(registerValidationRules), controller(authController.register));
+router.post("/register/admin", authorize("ADMIN"), validate(registerAdminValidationRules), controller(authController.registerAdmin));
 router.post("/login", validate(loginValidationRules), controller(authController.login));
 
 module.exports = router;
